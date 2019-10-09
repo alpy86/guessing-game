d47c7808d09e99bf18928e3d27c7b601b78b35fd
Alexandre Pylaev <alpy86@tut.by>
2019-10-09 18:10:17 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+   constructor() {}
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
-
+        return Math.round((this._min + this._max) / 2);
     }
 
     lower() {
-
+        this._max = this.guess();
     }
 
     greater() {
-
+        this._min = this.guess();
     }
 }
 
